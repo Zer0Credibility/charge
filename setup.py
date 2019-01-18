5862c5870059d80d7f4187e33bd642ad5f129acf
Zer0Credibility <cmr57@cam.ac.uk>
2019-01-18 18:17:43 +0000

    Updated Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='charge',
-      version='0.5.a.dev',
+      version='a0.5_dev2',
       description='Module for analysis of potentiostatic data',
       author='Clayton M. Rabideau',
       author_email='cmr57@cam.ac.uk',
